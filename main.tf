dd74bb91d63a596e0da71c6ce38fd6e444925c84
lucjross <2876576+lucjross@users.noreply.github.com>
2020-02-28 03:54:10 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ variable "cmd" {
   description = "The command used to create the resource."
 }
 
-variable "destroy_cmd" {
-  description = "The command used to destroy the resource."
-}
-
 variable "account_id" {
   description = "The account that holds the role to assume in. Will use providers account by default"
   default     = "0"
@@ -18,7 +14,7 @@ variable "role" {
 
 variable "dependency_ids" {
   description = "IDs or ARNs of any resources that are a dependency of the resource created by this module."
-  type        = "list"
+  type        = list
   default     = []
 }
 
@@ -31,27 +27,22 @@ locals {
 
 resource "null_resource" "cli_resource" {
   provisioner "local-exec" {
-    when    = "create"
+    when    = create
     command = "/bin/bash -c '${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.cmd}'"
-  }
-
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "/bin/bash -c '${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}'"
   }
 
   # By depending on the null_resource, the cli resource effectively depends on the existance
   # of the resources identified by the ids provided via the dependency_ids list variable.
-  depends_on = ["null_resource.dependencies"]
+  depends_on = [null_resource.dependencies]
 }
 
 resource "null_resource" "dependencies" {
-  triggers {
-    dependencies = "${join(",", var.dependency_ids)}"
+  triggers = {
+    dependencies = join(",", var.dependency_ids)
   }
 }
 
 output "id" {
   description = "The ID of the null_resource used to provison the resource via cli. Useful for creating dependencies between cli resources"
-  value       = "${null_resource.cli_resource.id}"
+  value       = null_resource.cli_resource.id
 }
